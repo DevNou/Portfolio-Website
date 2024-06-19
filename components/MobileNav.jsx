8c95510aa5e7e877fd942053044cee337eda65de
Arsenis Chatziantoniou <arsenixatzi@hotmail.com>
2024-06-19 22:58:27 +0300

    NavBarTransitionAndComponents

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -36,10 +36,29 @@ return <Sheet>
 </SheetTrigger>
 <SheetContent className="flex flex-col">
     {/* {logo} */}
-   <div>logo</div>
+   <div className="mt-32 mb-40 text-center text-2xl">
+    <Link href="/">
+    <h1 className="text-3xl font-semibold">Arsenis <span className="text-accent">.</span></h1></Link>
+   </div>
+   {/* {nav} */}
+   <nav className="flex flex-col justify-center items-center gap-8">
+    {links.map((link,index)=>{
+    return (
+        <Link href={link.path} key={index} 
+        className={`${
+            link.path === pathname && 
+        "text-accent border-b-2 border-accent"} 
+        text-xl capitalize
+        hover:text-accent transition-all`}
+        >
+            {link.name}
+        </Link>
+    );
+})}
+   </nav>
 </SheetContent>
-</Sheet>;
-  
-}
+</Sheet>
+  ;
+};
 
 export default MobileNav
